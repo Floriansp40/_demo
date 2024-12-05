b2f83ad1f0184ef044dbebc375b29860005de365
Florian <florian@ROGER>
2024-12-05 19:01:05 +0100

    test with only button

--- frontend/cypress/e2e/test_connection.cy.js
+++ frontend/cypress/e2e/test_connection.cy.js
@@ -10,12 +10,12 @@ describe("TEST LOGIN FORM", () => {
         cy.get('[data-cy="admin-link"]').click()
         cy.get('[data-cy="admin-login"]').should("be.visible")
 
-        cy.get('[data-cy="admin-login"').click()
+        // cy.get('[data-cy="admin-login"').click()
 
-        cy.get('[data-cy="admin-header"]').should('be.visible')
+        // cy.get('[data-cy="admin-header"]').should('be.visible')
 
-        cy.get('[data-cy=admin-logout]').click()
-        cy.get('[data-cy=home-page]').should('be.visible')
+        // cy.get('[data-cy=admin-logout]').click()
+        // cy.get('[data-cy=home-page]').should('be.visible')
 
     })
 
