672c71e1b47c6d9c3d33e9be99597b09b14263fc
Florian <florian@ROGER>
2024-12-05 19:24:47 +0100

    full test docker cypress

--- frontend/cypress/e2e/test_connection.cy.js
+++ frontend/cypress/e2e/test_connection.cy.js
@@ -2,20 +2,20 @@
 
 describe("TEST LOGIN FORM", () => {
     beforeEach(() => {
-        cy.visit('http://localhost:5173/auth/login')
+        cy.visit('http://localhost:5173')
     })
 
     it("Should login and redirect to admin page", () => {
 
-        // cy.get('[data-cy="admin-link"]').click()
+        cy.get('[data-cy="admin-link"]').click()
         cy.get('[data-cy="admin-login"]').should("be.visible")
 
-        // cy.get('[data-cy="admin-login"').click()
+        cy.get('[data-cy="admin-login"').click()
 
-        // cy.get('[data-cy="admin-header"]').should('be.visible')
+        cy.get('[data-cy="admin-header"]').should('be.visible')
 
-        // cy.get('[data-cy=admin-logout]').click()
-        // cy.get('[data-cy=home-page]').should('be.visible')
+        cy.get('[data-cy=admin-logout]').click()
+        cy.get('[data-cy=home-page]').should('be.visible')
 
     })
 
