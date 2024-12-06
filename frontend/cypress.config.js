3c95d54a86338e1ab3819ce38f962a0414c116ee
Florian <florian@ROGER>
2024-12-06 15:00:48 +0100

    sonar pon front

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here      
     },
   },
 });
